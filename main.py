0eb9a5bf53fc5faef9cafc4931311ceadb05920b
Adrian Salazar <113551883+asalazar23@users.noreply.github.com>
2022-10-07 19:47:05 +0000

    character gamble thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # random
-from random import randint
+from random import randint, choice
 # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
 #show you two useful functions for now.
 # print("random")
@@ -15,15 +15,30 @@ dice7 = randint(1,6)
 
 my_gamble = dice1 + dice2 + dice3 + dice4 + dice5 + dice6 + dice7
 
-if my_gamble == 25:
-  print("you win")
-elif my_gamble == 8:
-  print("roll again")
-elif my_gamble % 2 == 1:
-  print("you almost won")
-elif my_gamble > 12 and my_gamble < 18:
-  print("you lose")
-print(my_gamble)
+# if my_gamble == 25:
+#   print("you win")
+# elif my_gamble == 8:
+#   print("roll again")
+# elif my_gamble % 2 == 1:
+#   print("you almost won")
+# elif my_gamble > 12 and my_gamble < 18:
+#   print("you lose")
+
+
+dictionary1 = {} #empty dictionary
+# color = ['blue', 'red', 'green', 'turquoise', 'purple']
+# my_random = choice(color)
+# print(my_random)
+my_characters = ['rogue', 'elf', 'knight', 'ninja', 'samurai']
+for character in my_characters:
+  if my_gamble == character[0]:
+    print(character)
+  else:
+    print(my_gamble)
+
+
+
+
   # from random import shuffle
   # # This shuffles the list "in-place" meaning it won't return
   # # anything, instead it will effect the list passed
